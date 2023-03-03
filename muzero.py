9fae79c9ea93c591236d030deeb8ba0800dbcef2
dvmixalkin <dvmixalkin@gmail.com>
2023-03-03 17:48:17 +0300

    add extra data and set device

--- muzero.py
+++ muzero.py
@@ -16,8 +16,10 @@ from muzero_v1.utils import launch_job
 from muzero_v1.mode import random_vs_random
 
 
-def make_connect4_config() -> MuZeroConfig:
+def make_connect4_config(batch_size=64, num_actors=5) -> MuZeroConfig:
     return make_board_game_config(
+        batch_size=batch_size,
+        num_actors=num_actors,
         action_space_size=7,
         max_moves=20,
         dirichlet_alpha=0.03,
@@ -36,6 +38,7 @@ def muzero(config_, device_):
 
     # Start n concurrent actor threads
     threads = list()
+
     for _ in range(config_.num_actors):
         t = threading.Thread(target=launch_job, args=(run_selfplay, config_, storage, replay_buffer))
         threads.append(t)
@@ -43,14 +46,15 @@ def muzero(config_, device_):
     # Start all threads
     for x in threads:
         x.start()
-    train_network(config_, storage, replay_buffer)
+    # run_selfplay(config_, storage, replay_buffer)
+    train_network(config_, storage, replay_buffer, device=device_)
     return storage.latest_network()
 
 
 if __name__ == '__main__':
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     print(device)
-    config = make_connect4_config()
+    config = make_connect4_config(batch_size=64, num_actors=1)
     vs_random_once = random_vs_random(config)
     print('random_vs_random = ', sorted(vs_random_once.items()), end='\n')
     network = muzero(config, device)
