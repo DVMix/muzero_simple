b5684c91f66be31a43c3b128f1ddd8bbcfbc1d3a
d.mihalkin <d.mihalkin@acti.ru>
2023-03-03 15:32:19 +0300

    removed

--- main_v1.py
+++
@@ -1,8 +0,0 @@
-from muzero_v1.sandbox import *
-
-
-if __name__ == '__main__':
-    config = make_connect4_config()
-    vs_random_once = random_vs_random(config)
-    print('random_vs_random = ', sorted(vs_random_once.items()), end='\n')
-    network = muzero(config)
